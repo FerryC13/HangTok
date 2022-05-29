f8ff0b4f423ca71b1fa55abcbbfa73cef8ebd81a
EmilyTJX <62940519+EmilyTJX@users.noreply.github.com>
2022-05-29 23:46:20 +0800

    Update Instr.js

--- src/pages/Instr.js
+++ src/pages/Instr.js
@@ -1,11 +1,52 @@
-import React from "react";
-
-function Instr() {
-	return (
-		<>
-			<div>How to play</div>
-		</>
-	);
-}
-
+import React from "react"; 
+ 
+function Instr() { 
+ return ( 
+  <> 
+   <div>How to play</div> 
+ 
+ 
+ 
+    
+     
+     <h1>HOW TO PLAY HANGTOK?</h1> 
+     To save the stickman, you have to guess a word, letter by letter. 
+      <br/> 
+       Every time you choose a letter wrongly, one part of the stickman will be drawn out. 
+       <br/> 
+        If you are unable to guess the word in 6 tries, the stickman will be killed! 
+      
+     
+ 
+       
+ 
+      <svg 
+       height="250" 
+       width="200" 
+       class="figure-container" 
+       transform="translate(500,0)" 
+      > 
+        
+       <line x1="100" y1="20" x2="180" y2="40" /> 
+       <line x1="100" y1="20" x2="100" y2="50" /> 
+       <line x1="180" y1="20" x2="180" y2="230" /> 
+       <line x1="150" y1="230" x2="200" y2="230" /> 
+     
+       <circle cx="100" cy="70" r="20" /> 
+      
+       <line x1="100" y1="90" x2="100" y2="150" /> 
+       
+       <line x1="100" y1="120" x2="80" y2="100" /> 
+       <line x1="100" y1="120" x2="120" y2="100" /> 
+       
+       <line x1="100" y1="150" x2="80" y2="180" /> 
+       <line x1="100" y1="150" x2="120" y2="180" /> 
+      </svg> 
+       
+      <button type="button" class="btn btn-primary" >Back to Menu</button> 
+ 
+     </> 
+     ); 
+} 
+ 
 export default Instr;
