d843644967eb47fafbe4669e5bb789447dc8c0c2
Fauzaan <fauzaan.mm@gmail.com>
2022-05-29 15:44:30 +0800

    Menu features update 1
    Must change the value of "left" in Navbar.css for the changes to take effect

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import Notification from "./components/Notification";
 import { showNotification as show } from "./helpers/helpers";
 import "./App.css";
 import axios from "axios";
+import Navbar from "./components/Navbar";
+//import { Router } from "express";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Instr from './pages/Instr'
 
 // const words = ["application", "programming", "interface", "wizard"];
 
@@ -15,87 +19,95 @@ import axios from "axios";
 let selectedWord = "";
 
 function App() {
-  const i = useRef(0);
-  const [playable, setPlayable] = useState(true);
-  const [correctLetters, setCorrectLetters] = useState([]);
-  const [wrongLetters, setWrongLetters] = useState([]);
-  const [showNotification, setshowNotification] = useState(false);
-  const [post, setPost] = useState({});
+	const i = useRef(0);
+	const [playable, setPlayable] = useState(true);
+	const [correctLetters, setCorrectLetters] = useState([]);
+	const [wrongLetters, setWrongLetters] = useState([]);
+	const [showNotification, setshowNotification] = useState(false);
+	const [post, setPost] = useState({});
 
-  useEffect(() => {
-    axios
-      .get("https://random-word-api.herokuapp.com/word")
-      .then((res) => {
-        if (i.current == 0) {
-          selectedWord = res.data[0];
-          console.log(selectedWord);
-          setPost(selectedWord);
-          i.current = i.current + 1;
-        }
-        // console.log(res);
-        // console.log(res.data[0]);
-        // console.log(post);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+	useEffect(() => {
+		axios
+			.get("https://random-word-api.herokuapp.com/word")
+			.then((res) => {
+				if (i.current == 0) {
+					selectedWord = res.data[0];
+					console.log(selectedWord);
+					setPost(selectedWord);
+					i.current = i.current + 1;
+				}
+				// console.log(res);
+				// console.log(res.data[0]);
+				// console.log(post);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 
-    const handleKeydown = (event) => {
-      {
-        const { key, keyCode } = event;
-        if (playable && keyCode >= 65 && keyCode <= 90) {
-          const letter = key.toLowerCase();
+		const handleKeydown = (event) => {
+			{
+				const { key, keyCode } = event;
+				if (playable && keyCode >= 65 && keyCode <= 90) {
+					const letter = key.toLowerCase();
 
-          if (post.includes(letter)) {
-            if (!correctLetters.includes(letter)) {
-              setCorrectLetters((currentLetters) => [
-                ...currentLetters,
-                letter,
-              ]);
-            } else {
-              show(setshowNotification);
-            }
-          } else {
-            if (!wrongLetters.includes(letter)) {
-              setWrongLetters((wrongLetters) => [...wrongLetters, letter]);
-            } else {
-              show(setshowNotification);
-            }
-          }
-        }
-      }
-    };
-    window.addEventListener("keydown", handleKeydown);
+					if (post.includes(letter)) {
+						if (!correctLetters.includes(letter)) {
+							setCorrectLetters((currentLetters) => [
+								...currentLetters,
+								letter,
+							]);
+						} else {
+							show(setshowNotification);
+						}
+					} else {
+						if (!wrongLetters.includes(letter)) {
+							setWrongLetters((wrongLetters) => [...wrongLetters, letter]);
+						} else {
+							show(setshowNotification);
+						}
+					}
+				}
+			}
+		};
+		window.addEventListener("keydown", handleKeydown);
 
-    return () => window.removeEventListener("keydown", handleKeydown);
-  }, [correctLetters, wrongLetters, playable]);
+		return () => window.removeEventListener("keydown", handleKeydown);
+	}, [correctLetters, wrongLetters, playable]);
 
-  function playAgain() {
-    setPlayable(true);
+	function playAgain() {
+		setPlayable(true);
 
-    setCorrectLetters([]);
-    setWrongLetters([]);
+		setCorrectLetters([]);
+		setWrongLetters([]);
 
-    i.current = 0;
-  }
-  return (
-    <>
-      <Header className="head" />
-      <div className="game-container">
-        <WrongLetters wrongLetters={wrongLetters} />
-        <Figure wrongLetters={wrongLetters} />
-        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
-      </div>
-      <Popup
-        correctLetters={correctLetters}
-        wrongLetters={wrongLetters}
-        selectedWord={selectedWord}
-        setPlayable={setPlayable}
-        playAgain={playAgain}
-      />
-      <Notification showNotification={showNotification} />
-    </>
-  );
+		i.current = 0;
+	}
+	return (
+		<>
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/" exact/>
+					<Route path="/instructions" component={Instr}/>
+				</Routes>
+			</Router>
+			<Header className="head" />
+
+			<div className="game-container">
+				<WrongLetters wrongLetters={wrongLetters} />
+				<Figure wrongLetters={wrongLetters} />
+				<Word selectedWord={selectedWord} correctLetters={correctLetters} />
+			</div>
+			<Popup
+				correctLetters={correctLetters}
+				wrongLetters={wrongLetters}
+				selectedWord={selectedWord}
+				setPlayable={setPlayable}
+				playAgain={playAgain}
+			/>
+			<Notification showNotification={showNotification} />
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/SidebarData.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import * as AiIcons from "react-icons/ai";
+import * as RiIcons from "react-icons/ri"
+
+
+export const SidebarData = [
+    {
+        title: 'Home',
+        path: '/home',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'How To Play',
+        path: '/Instr',
+        icon: <RiIcons.RiQuestionMark />,
+        cName: 'nav-text'
+    },
+    
+
+
+
+
+]
+
+
